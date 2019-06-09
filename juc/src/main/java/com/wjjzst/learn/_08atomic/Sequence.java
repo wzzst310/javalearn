0db6c642821365acbb455f99d028cb6bb2c0b5d0
“wzzst310” <wzzst310@163.com>
2019-06-10 00:51:00 +0800

    aqs

--- juc/src/main/java/com/wjjzst/learn/_08atomic/Sequence.java
+++ juc/src/main/java/com/wjjzst/learn/_08atomic/Sequence.java
@@ -1,9 +1,6 @@
 package com.wjjzst.learn._08atomic;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicIntegerArray;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.*;
 
 /**
  * @Author: Wjj
@@ -19,8 +16,12 @@ public class Sequence {
     // AtomicLong;
 
     // 原子性更新数组
-    private int[] s = {1,2,3,4,5};
+    private int[] s = {1, 2, 3, 4, 5};
     AtomicIntegerArray as = new AtomicIntegerArray(s);
+    AtomicReference<User> user = new AtomicReference<>();
+    // 更新一个类的Integer类型的字段
+    AtomicIntegerFieldUpdater<User> age = AtomicIntegerFieldUpdater.newUpdater(User.class,"age");
+
     //as.
     // 原子性更新抽象类型
     // 原子性更新字段
@@ -28,7 +29,20 @@ public class Sequence {
 
     public int getNext() {
         s[2] = 10;
-        as.addAndGet(2,10);
+        as.addAndGet(2, 10);
+        as.getAndIncrement(2);
+        as.getAndAdd(2, 10);
+        User u = new User();
+        u.setAge(9);
+        this.user.getAndSet(u);
+        age.getAndIncrement(u);
+        System.out.println(u);
+        age.getAndIncrement(u);
+        System.out.println(u);
+        age.getAndIncrement(u);
+        System.out.println(u);
+
+        // 对User的实例SetandGet
         // value.incrementAndGet(); // 自增并且获取 ++ value
         // value.getAndIncrement() // 获取并且自增 value++
         return value.addAndGet(10);
@@ -37,7 +51,8 @@ public class Sequence {
 
     public static void main(String[] args) {
         Sequence s = new Sequence();
-        new Thread(new Runnable() {
+        s.getNext();
+        /*new Thread(new Runnable() {
             @Override
             public void run() {
                 while (true) {
@@ -75,6 +90,6 @@ public class Sequence {
                     }
                 }
             }
-        }, "线程三").start();
+        }, "线程三").start();*/
     }
 }
