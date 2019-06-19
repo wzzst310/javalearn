c93f637c9e4f221421e071304e6b9c2ca9a18334
wjj <wzzst310@163.com>
2019-06-20 07:44:25 +0800

    condition

--- juc/src/main/java/com/wjjzst/juc/learn/_13producterAndConsumer/Main.java
+++ juc/src/main/java/com/wjjzst/juc/learn/_13producterAndConsumer/Main.java
@@ -7,7 +7,8 @@ package com.wjjzst.juc.learn._13producterAndConsumer;
  */
 public class Main {
     public static void main(String[] args) {
-        Tmall tmall = new Tmall();
+        // Tmall tmall = new Tmall();
+        Tmall tmall = new Tmall1();
         PushTarget p = new PushTarget(tmall);
         TakeTarget t = new TakeTarget(tmall);
         new Thread(p).start();
