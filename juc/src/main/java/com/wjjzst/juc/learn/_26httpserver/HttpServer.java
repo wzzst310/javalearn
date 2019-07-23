4717389270f05bcb142c9594487cd824fc4ea914
wjj <wzzst310@163.com>
2019-07-23 08:45:23 +0800

    简单的web服务

--- juc/src/main/java/com/wjjzst/juc/learn/_26httpserver/HttpServer.java
+++ juc/src/main/java/com/wjjzst/juc/learn/_26httpserver/HttpServer.java
@@ -1,8 +1,6 @@
 package com.wjjzst.juc.learn._26httpserver;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,13 +13,38 @@ public class HttpServer {
     public static void main(String[] args) throws IOException {
         // 启动服务器 监听8888端口
         ServerSocket server = new ServerSocket(8888);
-        while (Thread.interrupted()) {
-            //不停接收客户端请求
+        System.out.println("服务器启动,监听" + 8888 + "端口");
+        while (!Thread.interrupted()) {
+            // 不停接收客户端请求
             Socket client = server.accept();
-            //获取输入流
+            System.out.println("客户端连接成功");
+            // 获取输入流
             InputStream ins = client.getInputStream();
             OutputStream out = client.getOutputStream();
+            // 读取请求内容
+            /*int len = 0;
+            byte[] b = new byte[1024];
+            while ((len = ins.read(b)) != -1) {
+                System.out.println(new String(b, 0, len));
+            }*/
+            BufferedReader reader = new BufferedReader(new InputStreamReader(ins));
+            String line = reader.readLine();
+            // 第一行请求行
+            System.out.println(line);
+            // 给用户响应
+            PrintWriter pw = new PrintWriter(out);
+            InputStream i = new FileInputStream("C:/Users/Administrator/IdeaProjects/javalearn/juc/src/main/resources/index.html");
+            BufferedReader fr = new BufferedReader(new InputStreamReader(ins));
+            pw.println("HTTP/1.1 200 OK");
+            pw.println("Content-Type: text/html;charset=utf-8");
+            pw.println("Content-Length: "+ i.available());
 
+            pw.println();
+            pw.flush();
+            String c = null;
+            while ((c = fr.readLine()) != null) {
+                pw.println(c);
+            }
         }
     }
 }
