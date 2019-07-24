38e0614ca319b7658c053d26df23207b9d1ec02d
wjj <wzzst310@163.com>
2019-07-24 08:40:37 +0800

    简单的web服务

--- juc/src/main/java/com/wjjzst/juc/learn/_26httpserver/HttpServer.java
+++ juc/src/main/java/com/wjjzst/juc/learn/_26httpserver/HttpServer.java
@@ -3,6 +3,7 @@ package com.wjjzst.juc.learn._26httpserver;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 /**
  * @Author: Wjj
@@ -12,8 +13,8 @@ import java.net.Socket;
 public class HttpServer {
     public static void main(String[] args) throws IOException {
         // 启动服务器 监听8888端口
-        ServerSocket server = new ServerSocket(8888);
-        System.out.println("服务器启动,监听" + 8888 + "端口");
+        ServerSocket server = new ServerSocket(9999);
+        System.out.println("服务器启动,监听" + 9999 + "端口");
         while (!Thread.interrupted()) {
             // 不停接收客户端请求
             Socket client = server.accept();
@@ -33,18 +34,22 @@ public class HttpServer {
             System.out.println(line);
             // 给用户响应
             PrintWriter pw = new PrintWriter(out);
-            InputStream i = new FileInputStream("C:/Users/Administrator/IdeaProjects/javalearn/juc/src/main/resources/index.html");
-            BufferedReader fr = new BufferedReader(new InputStreamReader(ins));
+            InputStream i = new FileInputStream("C:/Users/Administrator/IdeaProjects/javalearn/juc/src/main/resources/index1.html");
+            BufferedReader fr = new BufferedReader(new InputStreamReader(i));
             pw.println("HTTP/1.1 200 OK");
             pw.println("Content-Type: text/html;charset=utf-8");
-            pw.println("Content-Length: "+ i.available());
-
-            pw.println();
+            pw.println("Content-Length: " + i.available());
+            pw.println("Server: hello");
+            pw.println("Date: " + new Date());
+            pw.println("");
             pw.flush();
             String c = null;
             while ((c = fr.readLine()) != null) {
-                pw.println(c);
+                pw.print(c);
             }
+            pw.flush();
+            client.close();
         }
+        server.close();
     }
 }
