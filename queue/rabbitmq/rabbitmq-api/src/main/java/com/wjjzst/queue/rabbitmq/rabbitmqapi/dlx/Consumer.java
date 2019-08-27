39b2efade159c9553f65937a004b14f880b21d3a
wjj <wzzst310@163.com>
2019-08-27 21:33:29 +0800

    rabbitmq

--- queue/rabbitmq/rabbitmq-api/src/main/java/com/wjjzst/queue/rabbitmq/rabbitmqapi/dlx/Consumer.java
+++ queue/rabbitmq/rabbitmq-api/src/main/java/com/wjjzst/queue/rabbitmq/rabbitmqapi/dlx/Consumer.java
@@ -1,8 +1,12 @@
-package com.wjjzst.queue.rabbitmq.rabbitmqapi.consumer;
+package com.wjjzst.queue.rabbitmq.rabbitmqapi.dlx;
 
-import com.rabbitmq.client.*;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -25,12 +29,19 @@ public class Consumer {
         // 3 通过connection创建一个channel
         Channel channel = connection.createChannel();
         // 4 声明交换机和队列   然后进行绑定设置    最后指定路由Key
-        String exchangeName = "test_consumer_exchange";
-        String routingKey = "consumer.#";
-        String queueName = "test_consumer_queue";
+        String exchangeName = "test_dlx_exchange";
+        String routingKey = "dlx.#";
+        String queueName = "test_dlx_queue";
         channel.exchangeDeclare(exchangeName, "topic", true, false, null);
-        channel.queueDeclare(queueName, true, false, false, null);
+        Map<String, Object> arguments = new HashMap<>();
+        // 要进行死信队列的声明
+        arguments.put("x-dead-letter-exchange", "dlx.exchange");
+        channel.queueDeclare(queueName, true, false, false, arguments);
         channel.queueBind(queueName, exchangeName, routingKey);
+        // 要进行死信队列的声明
+        channel.exchangeDeclare("dlx.exchange", "topic", true, false, null);
+        channel.queueDeclare("dlx.queue", true, false, false, null);
+        channel.queueBind("dlx.queue", "dlx.exchange", "#");
         // 6 设置Channel
         // 手工签收必须关闭autoAck
         channel.basicConsume(queueName, true, new MyConsumer(channel));
