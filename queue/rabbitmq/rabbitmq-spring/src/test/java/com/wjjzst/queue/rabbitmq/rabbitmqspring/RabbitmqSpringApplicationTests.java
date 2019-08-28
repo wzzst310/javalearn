620d47f7d2cef7a31e0885d11ac295df075bb508
wjj <wzzst310@163.com>
2019-08-28 08:44:02 +0800

    rabbitmq

--- queue/rabbitmq/rabbitmq-spring/src/test/java/com/wjjzst/queue/rabbitmq/rabbitmqspring/RabbitmqSpringApplicationTests.java
+++ queue/rabbitmq/rabbitmq-spring/src/test/java/com/wjjzst/queue/rabbitmq/rabbitmqspring/RabbitmqSpringApplicationTests.java
@@ -2,12 +2,15 @@ package com.wjjzst.queue.rabbitmq.rabbitmqspring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.SQLOutput;
 import java.util.HashMap;
 
 @RunWith(SpringRunner.class)
@@ -42,6 +45,37 @@ public class RabbitmqSpringApplicationTests {
                 .to(new FanoutExchange("test.fanout", false, false))
         );
         //清空队列数据
-        rabbitAdmin.purgeQueue("test.topic.queue",false);
+        rabbitAdmin.purgeQueue("test.topic.queue", false);
+    }
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void testSendMessage() throws Exception {
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.getHeaders().put("desc", "信息描述");
+        messageProperties.getHeaders().put("type", "自定义消息类型。。。");
+        Message message = new Message("Hello RabbitMQ".getBytes(), messageProperties);
+        rabbitTemplate.convertAndSend("topic001", "spring.amqp", message, new MessagePostProcessor() {
+            @Override
+            public Message postProcessMessage(Message message) throws AmqpException {
+                System.out.println("----------添加额外的设置----------");
+                message.getMessageProperties().getHeaders().put("desc", "额外修改的信息描述");
+                message.getMessageProperties().getHeaders().put("attr", "额外新加的属性");
+
+                return message;
+            }
+        });
+    }
+
+    @Test
+    public void testSendMessage1() throws Exception {
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setContentType("text/plain");
+        Message message = new Message("mq 消息1234".getBytes(), messageProperties);
+        rabbitTemplate.send("topic001", "spring.abc", message);
+        rabbitTemplate.convertAndSend("topic001", "spring.amqp", "hello object message send!");
+        rabbitTemplate.convertAndSend("topic002", "rabbit.abc", "hello object message send!");
     }
 }
