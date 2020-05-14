2d22cc21e312d74744c9cc1d8b564d9a149bceef
wzzst310 <39961347+wzzst310@users.noreply.github.com>
2020-05-14 14:30:45 +0800

    ribbon 全局负载均衡配置

--- springcloud/consumer/consumer-ribbon-loadbalance/src/main/java/com/wjjzst/springcloud/consumer/ribbon/loadbalance/MyRibbonConfiguration.java
+++ springcloud/consumer/consumer-ribbon-loadbalance/src/main/java/com/wjjzst/springcloud/consumer/ribbon/loadbalance/MyRibbonConfiguration.java
@@ -4,6 +4,7 @@ import com.netflix.client.config.IClientConfig;
 import com.netflix.loadbalancer.IRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * @Author: Wjj
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
  * @desc:
  */
 @Configuration
+// @Profile("dev")
 public class MyRibbonConfiguration {
     @Bean
     public IRule ribbonRule() {
