96a782181441296067b19eee6b9c1149b2dce9eb
wjj <wzzst310@163.com>
2020-05-18 23:44:57 +0800

    dashboard hello

--- springcloud/dashboard/dashboard-hello/src/main/java/com/wjjzst/springcloud/dashboard/hello/DashboardHelloApplication.java
+++ springcloud/dashboard/dashboard-hello/src/main/java/com/wjjzst/springcloud/dashboard/hello/DashboardHelloApplication.java
@@ -1,12 +1,9 @@
 package com.wjjzst.springcloud.dashboard.hello;
 
-import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.context.annotation.Bean;
 
 /**
  * @Author: Wjj
