c4290e05dd4037de12c2519bc57e63e17a780a0d
wjj <wzzst310@163.com>
2020-10-01 01:30:10 +0800

    shardingsphere重新整合及sharding-proxy验证

---
+++ db/mysql/sharding-sphere/src/test/java/com/wjjzst/db/mysql/shardingsphere/ShardingSphereApplicationTests.java
@@ -0,0 +1,160 @@
+package com.wjjzst.db.mysql.shardingsphere;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.wjjzst.db.mysql.shardingsphere.entity.Course;
+import com.wjjzst.db.mysql.shardingsphere.entity.Order;
+import com.wjjzst.db.mysql.shardingsphere.entity.Udict;
+import com.wjjzst.db.mysql.shardingsphere.entity.User;
+import com.wjjzst.db.mysql.shardingsphere.mapper.CourseMapper;
+import com.wjjzst.db.mysql.shardingsphere.mapper.OrderMapper;
+import com.wjjzst.db.mysql.shardingsphere.mapper.UdictMapper;
+import com.wjjzst.db.mysql.shardingsphere.mapper.UserMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.UUID;
+
+/**
+ * @Author: Wjj
+ * @Date: 2020/9/17 1:17 上午
+ * @desc:
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShardingSphereApplicationTests {
+
+    //注入mapper
+    @Autowired
+    private CourseMapper courseMapper;
+
+    //注入user的mapper
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private UdictMapper udictMapper;
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+
+    //======================测试shardingproxy==================
+    //添加操作
+    @Test
+    public void addOrderDb() {
+        for (int i = 0; i < 2000; i++) {
+            Order order = new Order();
+            //int orderId = (int) (Math.random() * 10000000);
+            int userId = (int) (Math.random() * 10000000);
+            order.setOrderId(i);
+            order.setUserId(userId);
+            order.setStatus(UUID.randomUUID().toString());
+            orderMapper.insert(order);
+        }
+    }
+
+    //查询操作
+    @Test
+    public void findOrderDb() {
+        QueryWrapper<Order> wrapper = new QueryWrapper<>();
+        //设置userid值
+        wrapper.eq("order_id", 1000);
+        Order order = orderMapper.selectOne(wrapper);
+        System.out.println(order);
+    }
+
+
+    //======================测试公共表===================
+    //添加操作
+    @Test
+    public void addDict() {
+        Udict udict = new Udict();
+        udict.setUstatus("c");
+        udict.setUvalue("已启用");
+        udictMapper.insert(udict);
+    }
+
+    //删除操作
+    @Test
+    public void deleteDict() {
+        QueryWrapper<Udict> wrapper = new QueryWrapper<>();
+        //设置userid值
+        wrapper.eq("dictid", 1309919818501120001L);
+        udictMapper.delete(wrapper);
+    }
+
+    //======================测试垂直分库==================
+    //添加操作
+    @Test
+    public void addUserDb() {
+        for (int i = 0; i < 10; i++) {
+            User user = new User();
+            user.setUsername("lucymary");
+            user.setUstatus("a");
+            userMapper.insert(user);
+        }
+
+    }
+
+    //查询操作
+    @Test
+    public void findUserDb() {
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        //设置userid值
+        wrapper.eq("user_id", 1310271995765641218L);
+        User user = userMapper.selectOne(wrapper);
+        System.out.println(user);
+    }
+
+
+    //======================测试水平分库=====================
+    //添加操作
+    @Test
+    public void addCourseDb() {
+        Course course = new Course();
+        course.setCname("javademo1");
+        //分库根据user_id
+        course.setUserId(110L);
+        course.setCstatus("Normal1");
+        courseMapper.insert(course);
+    }
+
+    //查询操作
+    @Test
+    public void findCourseDb() {
+        QueryWrapper<Course> wrapper = new QueryWrapper<>();
+        //设置userid值
+        wrapper.eq("user_id", 110L);
+        //设置cid值
+        wrapper.eq("cid", 1308466072415977473L);
+        Course course = courseMapper.selectOne(wrapper);
+        System.out.println(course);
+    }
+
+    //=======================测试水平分表===================
+    //添加课程的方法
+    @Test
+    public void addCourse() {
+        for (int i = 1; i <= 10; i++) {
+            Course course = new Course();
+            course.setCname("java" + i);
+            course.setUserId(100L);
+            course.setCstatus("Normal" + i);
+            courseMapper.insert(course);
+        }
+    }
+
+    //查询课程的方法
+    @Test
+    public void findCourse() {
+        QueryWrapper<Course> wrapper = new QueryWrapper<>();
+        wrapper.eq("cid", 1307155339149357057L);
+        Course course = courseMapper.selectOne(wrapper);
+        System.out.println(course);
+    }
+
+
+}
