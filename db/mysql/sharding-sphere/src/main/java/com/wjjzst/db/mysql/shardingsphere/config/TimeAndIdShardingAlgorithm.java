58669c941aba9f69f4c11fb88afb562a26b06d4c
wjj <wangzhe2@pconline.com.cn>
2020-11-22 18:13:58 +0800

    分库分表

--- db/mysql/sharding-sphere/src/main/java/com/wjjzst/db/mysql/shardingsphere/config/TimeAndIdShardingAlgorithm.java
+++ db/mysql/sharding-sphere/src/main/java/com/wjjzst/db/mysql/shardingsphere/config/TimeAndIdShardingAlgorithm.java
@@ -1,6 +1,8 @@
 package com.wjjzst.db.mysql.shardingsphere.config;
 
+import com.alibaba.fastjson.JSON;
 import com.wjjzst.common.util.DateUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.shardingsphere.api.sharding.complex.ComplexKeysShardingAlgorithm;
 import org.apache.shardingsphere.api.sharding.complex.ComplexKeysShardingValue;
 
@@ -11,24 +13,39 @@ import java.util.*;
  * @Date: 2020/11/22 13:47
  * @desc:
  */
+@Slf4j
 public class TimeAndIdShardingAlgorithm<T extends Comparable<?>> implements ComplexKeysShardingAlgorithm<T> {
 
     @Override
     public Collection<String> doSharding(Collection<String> availableTargetNames, ComplexKeysShardingValue<T> shardingValues) {
         System.out.println("collection:" + availableTargetNames + ",shardingValues:" + shardingValues);
         Map columnNameAndShardingValuesMap = shardingValues.getColumnNameAndShardingValuesMap();
-        Collection<Integer> cardNos = (Collection<Integer>) columnNameAndShardingValuesMap.get("cardNo");
-        Collection<Date> createAts = (Collection<Date>) columnNameAndShardingValuesMap.get("createAt");
+        Collection<Long> cardNos = (Collection<Long>) columnNameAndShardingValuesMap.get("card_no");
+        Collection<Date> createAts = (Collection<Date>) columnNameAndShardingValuesMap.get("create_at");
         List<String> shardingSuffix = new ArrayList<>();
-        System.out.println(cardNos);
-        System.out.println(createAts);
+        log.error("cardNos: " + JSON.toJSONString(cardNos));
+        log.error("createAts: " + JSON.toJSONString(createAts));
         // user_id，order_id分片键进行分表
-        for (Integer cardNo : cardNos) {
+//        Long cardNo = cardNos.iterator().next();
+//        Date createAt = createAts.iterator().next();
+//        long cardNoPrefix = (cardNo % 3) + 1;
+//        String createAtPrefix = DateUtils.date2Str(createAt, DateUtils.YEAR_MONTH_NUMBER);
+//        log.error("cardNo: " + cardNoPrefix);
+//        log.error("createAt: " + createAtPrefix);
+//        String suffix = "_" + cardNoPrefix + "_" + createAtPrefix;
+//        for (String s : availableTargetNames) {
+//            if (s.endsWith(suffix)) {
+//                shardingSuffix.add(s);
+//            }
+//        }
+//        return shardingSuffix;
+//
+        for (Long cardNo : cardNos) {
             for (Date createAt : createAts) {
-                int cardNoPrefix = (cardNo % 3) + 1;
+                long cardNoPrefix = (cardNo % 3) + 1;
                 String createAtPrefix = DateUtils.date2Str(createAt, DateUtils.YEAR_MONTH_NUMBER);
-                System.out.println("cardNo: " + cardNoPrefix);
-                System.out.println("createAt: " + createAtPrefix);
+                log.error("cardNo: " + cardNoPrefix);
+                log.error("createAt: " + createAtPrefix);
                 String suffix = "_" + cardNoPrefix + "_" + createAtPrefix;
                 for (String s : availableTargetNames) {
                     if (s.endsWith(suffix)) {
--- db/mysql/sharding-sphere/src/test/java/com/wjjzst/db/mysql/shardingsphere/ShardingSphereApplicationTests.java
+++ db/mysql/sharding-sphere/src/test/java/com/wjjzst/db/mysql/shardingsphere/ShardingSphereApplicationTests.java
@@ -168,7 +168,7 @@ public class ShardingSphereApplicationTests {
             record.setCname("java" + i);
             record.setUserId(100L);
             record.setCstatus("Normal" + i);
-            Date date = randomDate("2020-01-01", "2023-12-31");
+            Date date = randomDate("2020-01-01", "2020-12-31");
             record.setCardNo((long) (Math.random() * 1000000000));
             record.setCreateTime(date);
             record.setCreateAt(date);
